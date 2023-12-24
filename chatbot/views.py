ff7030d32ad5e3fd90365cc14f8f3dd2ae52360e
raphaelgpalma <raphaelgpalma@icloud.com>
2023-12-24 17:06:00 -0300

    creating consumers.py for progressive response

--- chatbot/views.py
+++ chatbot/views.py
@@ -13,6 +13,7 @@ from datetime import datetime, timedelta, time
 import unicodedata
 from dotenv import load_dotenv
 import os
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 load_dotenv()
 
